d5ddfa684daf2d606ea5aa4b91f25c6c5563397e
Eirik Ola Aksnes <eoaksnes@gmail.com>
2015-08-29 13:02:49 +0200

    Show code solutions in own branch

--- src/01-classes/class.js
+++ src/01-classes/class.js
@@ -1,15 +1,3 @@
-/*function Point(x,y) {
-    this.x = x;
-    this.y = y;
-}
-
-Point.prototype.toString = function () {
-    return this.x + ' ' + this.y;
-};
-
-module.exports = Point;*/
-
-
 class Point {
     constructor(x, y) {
         this.x = x;
