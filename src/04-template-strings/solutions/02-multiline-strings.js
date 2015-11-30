623364dc10122e639b31086d43755d3613176008
Magne Rodem <marode@statoil.com>
2015-11-30 21:30:49 +0100

    Solutions for template strings

---
+++ src/04-template-strings/solutions/02-multiline-strings.js
@@ -0,0 +1,12 @@
+var prefix = 'Never gonna';
+
+function printLyrics() {
+    return `${prefix} give you up
+${prefix} let you down
+${prefix} run around and desert you
+${prefix} make you cry
+${prefix} say goodbye
+${prefix} tell a lie and hurt you`;
+}
+
+module.exports = printLyrics;
